e5e0451c8d6b1f97faf1702335fdb0ad68419c3b
Aaditya Prakash Kattekola <kattek811981@student.nitw.ac.in>
2021-04-24 19:29:57 +0400

    lsp-config .vim -> .lua & Add Neogit

--- plugins_config/languages/rust-config.lua
+++ plugins_config/languages/rust-config.lua
@@ -1,1 +1,2 @@
 require'lspconfig'.rust_analyzer.setup({})
+vim.cmd("autocmd InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost *.rs :lua require'lsp_extensions'.inlay_hints{ prefix = ' » ', highlight = 'NonText', enabled = {'ChainingHint'} }")
