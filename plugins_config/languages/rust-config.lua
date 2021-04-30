9e1f1bba2c6b39be5c090eb486673e1dd56e6780
Aaditya Prakash Kattekola <kattek811981@student.nitw.ac.in>
2021-04-30 22:13:35 +0400

    ADD rust-analyzer

--- plugins_config/languages/rust-config.lua
+++ plugins_config/languages/rust-config.lua
@@ -1,1 +1,5 @@
-require'lspconfig'.rls.setup{ on_attach=require'completion'.on_attach }
+local rust_analyzer_path = vim.fn.stdpath('data')..'/lsp-servers/rust-analyzer/rust-analyzer.exe'
+require'lspconfig'.rust_analyzer.setup{
+    cmd = {rust_analyzer_path},
+    on_attach=require'completion'.on_attach
+}
